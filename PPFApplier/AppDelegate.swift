6ecdbe30139cb35327d9a02f897fc324dbec0e60
Victor Maia Aldecoa <victor.aldecoa@tfgco.com>
2017-03-05 12:54:34 -0300

    Add main menu template

---
+++ PPFApplier/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  PPFApplier
+//
+//  Created by Victor Maia Aldecoa on 05/03/17.
+//  Copyright © 2017 victoraldecoa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    }
+
+
+}
+
